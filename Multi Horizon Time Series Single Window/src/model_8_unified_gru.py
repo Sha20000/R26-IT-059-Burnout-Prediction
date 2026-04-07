995320f122315c5688e6f871ed85c3d8926f01e2
Sha20000 <shanukamaduranga2000@gmail.com>
2026-04-07 13:43:59 +0530

    Finished device setup

--- Multi Horizon Time Series Single Window/src/model_8_unified_gru.py
+++ Multi Horizon Time Series Single Window/src/model_8_unified_gru.py
@@ -42,3 +42,9 @@ torch.manual_seed(SEED)
 np.random.seed(SEED)
 
 #Device setup
+if torch.cuda.is_available():
+    device = torch.device('cuda')
+elif torch.backends.mps.is_available():
+    device = torch.device('mps')
+else:
+    device = torch.device('cpu')
